004122a115e43164af5ddd1421996631d35310fc
Miraz <intern04@atilimited.net>
2024-06-27 18:22:58 +0600

    crud complete

---
+++ src/Data.jsx
@@ -0,0 +1,22 @@
+export const userList = [
+  {
+    name: "abc",
+    email: "abc@gmail.com",
+    id: 1,
+  },
+  {
+    name: "abcd",
+    email: "abcd@gmail.com",
+    id: 2,
+  },
+  {
+    name: "abcde",
+    email: "abcde@gmail.com",
+    id: 3,
+  },
+  {
+    name: "abcdef",
+    email: "abcdef@gmail.com",
+    id: 4,
+  },
+];
